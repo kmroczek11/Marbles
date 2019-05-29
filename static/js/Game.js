d7d0e8b44e5d8743266b3d52131f0a8081c4f84f
kmroczek11 <kamil.mroczek@onet.eu>
2019-05-29 20:16:43 +0200

    Created walls around the plane

--- static/js/Game.js
+++ static/js/Game.js
@@ -20,8 +20,9 @@ class Game {
     this.renderer.setSize($(window).width(), $(window).height());
     $("#root").append(this.renderer.domElement);
     var axes = new THREE.AxesHelper(1000);
+    axes.position.set(0, 25, 0); //podniesienie osi, aby były widoczne
     this.scene.add(axes);
-    this.camera.position.set(1500, 1500, 0);
+    this.camera.position.set(0, 2500, 2500);
     this.camera.lookAt(this.scene.position);
     this.render();
 
@@ -45,10 +46,38 @@ class Game {
     );
     platform.rotation.x = Math.PI / 2;
     this.scene.add(platform);
+    this.createEdges();
   }
 
   render() {
     this.renderer.render(this.scene, this.camera);
     requestAnimationFrame(this.render.bind(this));
+  }
+
+  createEdges() {
+    var singleGeometry = new THREE.Geometry();
+    for (let i = 0; i < 4; i++) {
+      var edge = new THREE.Mesh(settings.edgeGeometry);
+      switch (i) {
+        case 0:
+          edge.position.set(0, 25, 975);
+          break;
+        case 1:
+          edge.position.set(0, 25, -975);
+          break;
+        case 2:
+          edge.position.set(-975, 25, 0);
+          edge.rotation.y = Math.PI / 2;
+          break;
+        case 3:
+          edge.position.set(975, 25, 0);
+          edge.rotation.y = Math.PI / 2;
+          break;
+      }
+      edge.updateMatrix(); // bez tego pozycja geometrii jest zawsze 0,0,0
+      singleGeometry.merge(edge.geometry, edge.matrix);
+    }
+    var edges = new THREE.Mesh(singleGeometry, settings.edgeMaterial);
+    this.scene.add(edges);
   }
 }
--- static/js/Settings.js
+++ static/js/Settings.js
@@ -1,8 +1,21 @@
 var settings = {
-  platformGeometry: new THREE.PlaneGeometry(1200, 1200, 50, 50),
+  platformGeometry: new THREE.PlaneGeometry(2000, 2000, 50, 50),
 
   platformMaterial: new THREE.MeshBasicMaterial({
     color: 0x000000,
     side: THREE.DoubleSide
+  }),
+
+  edgeGeometry: new THREE.BoxGeometry(2000, 50, 50),
+
+  edgeMaterial: new THREE.MeshBasicMaterial({
+    color: 0xd3d3d3,
+    side: THREE.DoubleSide
+  }),
+
+  wallGeometry: new THREE.BoxGeometry(200, 50, 50),
+  wallMaterial: new THREE.MeshBasicMaterial({
+    side: THREE.DoubleSide,
+    map: new THREE.TextureLoader().load("../gfx/wall.jpg")
   })
 };
